2bb5324f81afc1cec49d5c9216cec2b39f6a51d2
Tanya <Tanya2204frosya@gmail.com>
2023-01-28 17:23:37 +0300

    adding dz

---
+++ Desktop/Seminar 2.1/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+System.Console.WriteLine("Enter the numbers");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+int number2 = Convert.ToInt32 (Console.ReadLine());
+
+if (number1 > number2)
+{
+    System.Console.WriteLine("Большое число " + number1 );
+}
+else
+{
+    System.Console.WriteLine("Большое число " + number2);
+}
+
