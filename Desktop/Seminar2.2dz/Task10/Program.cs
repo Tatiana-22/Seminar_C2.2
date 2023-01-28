d325f958e94d1de3a9634833c5bfe92ef3a3814c
Tanya <Tanya2204frosya@gmail.com>
2023-01-28 23:14:15 +0300

    task10

---
+++ Desktop/Seminar2.2dz/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1/10;
+int result = num2%10;
+
+System.Console.WriteLine($"{num1}->{result}");
