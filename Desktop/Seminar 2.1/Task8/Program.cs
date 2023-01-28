2cc84fa4dd99f07d50bbd068cadeaab1b5db605d
Tanya <Tanya2204frosya@gmail.com>
2023-01-28 17:34:32 +0300

    task8

---
+++ Desktop/Seminar 2.1/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.WriteLine("Enter the number");
+int numberN = Convert.ToInt32 (Console.ReadLine());
+int currentNumber = 1;
+
+while (currentNumber <= numberN)
+{
+    if(currentNumber%2==0)
+    System.Console.WriteLine(currentNumber);
+    currentNumber++;
+}
